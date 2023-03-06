02f8cf22da872ef1e1e53f15929af08a6dfca7b3
Gustavo de Souza <gustavo.de.souza@hotmail.com>
2023-03-06 13:37:02 -0300

    Subindo CRUD

---
+++ crudLaravel/resources/views/companies/edit.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Editar Empresa</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left">
+                    <h2>Editar Empresa</h2>
+                </div>
+                <div class="pull-right">
+                    <a class="btn btn-primary" href="{{ route('companies.index') }}" enctype="multipart/form-data">
+                        Voltar</a>
+                </div>
+            </div>
+        </div>
+        @if(session('status'))
+        <div class="alert alert-success mb-1 mt-1">
+            {{ session('status') }}
+        </div>
+        @endif
+        <form action="{{ route('companies.update',$company->id) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Nome da Empresa:</strong>
+                        <input type="text" name="name" value="{{ $company->name }}" class="form-control"
+                            placeholder="Nome da Empresa...">
+                        @error('name')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Email da empresa:</strong>
+                        <input type="email" name="email" class="form-control" placeholder="Email da empresa"
+                            value="{{ $company->email }}">
+                        @error('email')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Endereço da empresa:</strong>
+                        <input type="text" name="address" value="{{ $company->address }}" class="form-control"
+                            placeholder="Endereço da empresa...">
+                        @error('address')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary ml-3">Enviar</button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
